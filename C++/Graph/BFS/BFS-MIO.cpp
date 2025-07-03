ec8c5d687135f84eb954331bcaea2f6f01b256e4
Eliodoro <eliodoro.mascolo001@studenti.uniparthenope.it>
2025-07-03 12:53:20 +0200

    Refactor graph BFS and input handling, add MST support
    Refactored BFS-MIO.cpp and es.cpp to use a Node/Edge/Graph class structure, improved BFS implementation, and added Prim's algorithm for MST. Updated input/output handling to read from formatted files, and replaced old input.txt/output.txt with new graph data and BFS results. Added sample input and output files for testing.

--- C++/Graph/BFS/BFS-MIO.cpp
+++ C++/Graph/BFS/BFS-MIO.cpp
@@ -1,220 +1,229 @@
-#include <stdio.h>
-#include <vector>
 #include <iostream>
+#include <vector>
 #include <fstream>
 #include <climits>
 #include <queue>
+#include <sstream>
 
 using namespace std;
 
-enum Color{white, gray, black};
+enum Color { white, gray, black };
 
-class Node{
+class Node {
+private:
+    Node* predecessor;
+    vector<Node*> adj;
+    Color color;
+    int value;
+    int distance;
 
-    private:
-        Node*predecessor;
-        vector<Node*> adj;
-        Color color;
-        int value;
-        int distance;
+public:
+    Node(int key) : value(key), predecessor(nullptr), color(white), distance(INT_MAX) {}
 
-    public:
+    int getValue() { return value; }
+    int getDistance() { return distance; }
+    Node* getPredecessor() { return predecessor; }
+    vector<Node*> getAdj() { return adj; }
+    Color getColor() { return color; }
 
-        Node(int key) : value(key), predecessor(nullptr), color(white), distance(INT_MAX){}
-
-        int getValue(){return value;}
-        int getDistance(){return distance;}
-        Node* getPredecessor(){return predecessor;}
-        vector<Node*> getAdj(){return adj;}
-        Color getColor(){return color;}
-
-        void setColor(Color c){c = color;}
-        void setPredecessor(Node* node){predecessor = node;}
-        void setAdj(Node* node){adj.push_back(node);}
-        void setDistance(int d){distance = d;}
+    void setColor(Color c) { color = c; }
+    void setPredecessor(Node* node) { predecessor = node; }
+    void setAdj(Node* node) { adj.push_back(node); }
+    void setDistance(int d) { distance = d; }
 };
 
-class Edge{
+class Edge {
+private:
+    Node* src;
+    Node* dest;
+    int weight;
 
-    private:
-        Node* src;
-        Node* dest;
-        int weight;
+public:
+    Edge(Node* u, Node* v, int w) : src(u), dest(v), weight(w) {}
 
-    public:
-        Edge(Node* u, Node* v, int w) : src(u), dest(v), weight(w){}
+    Node* getSrc() { return src; }
+    Node* getDest() { return dest; }
+    int getWeight() { return weight; }
 
-        Node* getSrc(){return src;}
-        Node* getDest(){return dest;}
-        int getWeight(){return weight;}
-
-        void setSrc(Node* u){src = u;}
-        void setDest(Node* v){dest = v;}
-        void setWeight(int w){weight = w;}
-
+    void setSrc(Node* u) { src = u; }
+    void setDest(Node* v) { dest = v; }
+    void setWeight(int w) { weight = w; }
 };
 
-
-class Graph{
-
-    private:
-        vector<Node*> nodes;
-        vector<Edge*> edges;
-        int V, E;
-
-        struct compare{
-            bool operator()(Node* u, Node* v){
-                return u->getDistance() > v->getDistance();
-            }
-        };
-        
-
-    public:
-        Graph(int v, int e) : V(v), E(e){}
-
-        void addNode(Node* node){
-            nodes.push_back(node);
-            if(nodes.size() > V){
-                V = nodes.size();
-            }
-        }
-
-        Node* getNode(int value){
-            for(auto& node : nodes){
-                if(node->getValue() == value){
-                    return node;
-                }
-            }
-            return nullptr;
-        }
-
-        void addEdge(Node* src, Node* dest, int weight){
-
-            edges.push_back(new Edge(src, dest, weight));
-            src->setAdj(dest);
-            dest->setAdj(src);
-
-            if(edges.size() > E){
-                E = edges.size();
-            }
-        }
-
-        Edge* getEdge(Node* src, Node* dest){
-            for(auto& edge : edges){
-                if(edge->getSrc() == src && edge->getDest() == dest)
-                    return edge;
-            }
-            return nullptr;
-        }
-
-
-        void BFS(Node* source, ofstream& out){
-
-            for(auto& node : nodes){
-                node->setColor(white);
-                node->setPredecessor(nullptr);
-                node->setDistance(INT_MAX);
-            }
-
-            source->setColor(gray);
-            source->setDistance(0);
-
-            queue<Node*> q;
-            q.push(source);
-
-            while(!q.empty()){
-
-                Node* node = q.front();
-                q.pop();
-
-                for(Node* adj : node->getAdj()){
-                    if(adj->getColor() == white){
-                        adj->setPredecessor(node);
-                        adj->setColor(gray);
-                        adj->setDistance(node->getDistance() + 1);
-
-                        q.push(adj);
-                    }
-                }
-
-                node->setColor(black);
-            }
-
-            for(auto& node : nodes){
-                if(node->getPredecessor() == nullptr)
-                    out << "Node: " << node->getValue() << " -> (predecessor: NULL, distance: " << node->getDistance() << ")"<<endl;
-                else
-                    out << "Node: "<< node->getValue() << " -> (predecessor: " << node->getPredecessor() << " distance: " << node->getDistance() << ")"<<endl;
-            }
-
-        }
-
-        void prim(Node* s){
-            for(auto& node : nodes){
-                node->setPredecessor(nullptr);
-                node->setDistance(INT_MAX);
-            }
-
-            s->setDistance(0);
-
-            vector<bool> inMST(V, false);
-            priority_queue<Node*, vector<Node*>, compare> minPQ;
-            minPQ.push(s);
-
-            while(!minPQ.empty()){
-                Node* node = minPQ.top();
-                minPQ.pop();
-                inMST[node->getValue()] = true;
-
-                for(auto& adj : node->getAdj()){
-                    Edge* edge = getEdge(node, adj);
-                    if(!inMST[adj->getValue()] && edge->getWeight() < adj->getDistance() ){
-                        adj->setDistance(edge->getWeight());
-                        adj->setPredecessor(node);
-                        minPQ.push(adj);
-                    }
-                }
-            }
-        }
-
-        void printMST(){
-            cout << " Minimum spanning tree:"<<endl;
-
-            for(auto& node : nodes){
-                if(node->getPredecessor() == nullptr)
-                    cout << "Il nodo con valore: " << node->getValue() << " -> (predecessor: NULL, distance: " << node->getDistance() << ")"<<endl;
-                else
-                    cout << "Il nodeo con valore: " << node->getValue() << "-> (predecessor: " << node->getPredecessor() << " distance: " << node->getDistance() <<")"<<endl;
-            }
-        }
-
-};
-
-
-int main(){
-
-    ifstream in("input.txt");
-    ofstream out("output.txt");
+class Graph {
+private:
+    vector<Node*> nodes;
+    vector<Edge*> edges;
     int V, E;
 
-    in >> V >> E;
+    struct compare {
+        bool operator()(Node* u, Node* v) {
+            return u->getDistance() > v->getDistance();
+        }
+    };
 
-    Graph graph(V,E);
-    for(int i = 0; i < V; i++){
-        graph.addNode(new Node(i));
+public:
+    Graph(int v = 0, int e = 0) : V(v), E(e) {}
+
+    void addNode(Node* node) {
+        nodes.push_back(node);
+        if (nodes.size() > V) V = nodes.size();
     }
 
-    for(int i = 0; i < V; i++){
-        int u, v, w;
-
-        in >> u >> v>> w;
-
-        Node* src = graph.getNode(u);
-        Node* dest = graph.getNode(v);
-
-        if(src != nullptr && dest != nullptr)
-            graph.addEdge(src, dest, w);
-        else
-            cout<< " Non e' stato possibile aggiungere l'arco poiche' il nodo "<< u << "e/o il nodo:" << v << "sono nullptr"<<endl;
+    Node* getNode(int value) {
+        for (auto& node : nodes)
+            if (node->getValue() == value)
+                return node;
+        return nullptr;
     }
+
+    void addEdge(Node* src, Node* dest, int weight) {
+        edges.push_back(new Edge(src, dest, weight));
+        src->setAdj(dest);
+        dest->setAdj(src);  // Se grafo orientato, rimuovi questa riga
+        if (edges.size() > E) E = edges.size();
+    }
+
+    Edge* getEdge(Node* src, Node* dest) {
+        for (auto& edge : edges)
+            if (edge->getSrc() == src && edge->getDest() == dest)
+                return edge;
+        return nullptr;
+    }
+
+    void BFS(Node* source, ofstream& out) {
+        for (auto& node : nodes) {
+            node->setColor(white);
+            node->setPredecessor(nullptr);
+            node->setDistance(INT_MAX);
+        }
+
+        source->setColor(gray);
+        source->setDistance(0);
+
+        queue<Node*> q;
+        q.push(source);
+
+        while (!q.empty()) {
+            Node* node = q.front();
+            q.pop();
+
+            for (Node* adj : node->getAdj()) {
+                if (adj->getColor() == white) {
+                    adj->setPredecessor(node);
+                    adj->setColor(gray);
+                    adj->setDistance(node->getDistance() + 1);
+                    q.push(adj);
+                }
+            }
+
+            node->setColor(black);
+        }
+
+        for (auto& node : nodes) {
+            if (node->getPredecessor() == nullptr)
+                out << "Node: " << node->getValue() << " -> (predecessor: NULL, distance: " << node->getDistance() << ")" << endl;
+            else
+                out << "Node: " << node->getValue() << " -> (predecessor: " << node->getPredecessor()->getValue()
+                    << ", distance: " << node->getDistance() << ")" << endl;
+        }
+    }
+
+    void prim(Node* s) {
+        for (auto& node : nodes) {
+            node->setPredecessor(nullptr);
+            node->setDistance(INT_MAX);
+        }
+
+        s->setDistance(0);
+        vector<bool> inMST(V, false);
+        priority_queue<Node*, vector<Node*>, compare> minPQ;
+        minPQ.push(s);
+
+        while (!minPQ.empty()) {
+            Node* node = minPQ.top();
+            minPQ.pop();
+            inMST[node->getValue()] = true;
+
+            for (auto& adj : node->getAdj()) {
+                Edge* edge = getEdge(node, adj);
+                if (edge && !inMST[adj->getValue()] && edge->getWeight() < adj->getDistance()) {
+                    adj->setDistance(edge->getWeight());
+                    adj->setPredecessor(node);
+                    minPQ.push(adj);
+                }
+            }
+        }
+    }
+
+    void printMST() {
+        cout << "Minimum spanning tree:" << endl;
+        for (auto& node : nodes) {
+            if (node->getPredecessor() == nullptr)
+                cout << "Nodo " << node->getValue() << " -> (predecessore: NULL, distanza: " << node->getDistance() << ")" << endl;
+            else
+                cout << "Nodo " << node->getValue() << " -> (predecessore: " << node->getPredecessor()->getValue()
+                     << ", distanza: " << node->getDistance() << ")" << endl;
+        }
+    }
+
+    void loadFromFile(ifstream& input) {
+        int totalNodes, totalEdges;
+        string headerLine;
+
+        getline(input, headerLine);
+        if (headerLine.front() == '<') headerLine = headerLine.substr(1);
+        if (headerLine.back() == '>') headerLine.pop_back();
+        for (char& c : headerLine) if (c == ',') c = ' ';
+
+        istringstream headerStream(headerLine);
+        headerStream >> totalNodes >> totalEdges;
+
+        for (int i = 0; i < totalNodes; ++i)
+            addNode(new Node(i));
+
+        string line;
+        while (getline(input, line)) {
+            if (line.front() == '<') line = line.substr(1);
+            if (line.back() == '>') line.pop_back();
+            for (char& c : line) if (c == ',') c = ' ';
+
+            istringstream edgeStream(line);
+            int srcVal, destVal, weight;
+            edgeStream >> srcVal >> destVal >> weight;
+
+            Node* src = getNode(srcVal);
+            Node* dest = getNode(destVal);
+            if (src && dest)
+                addEdge(src, dest, weight);
+        }
+    }
+};
+
+// === MAIN ===
+
+int main() {
+    ifstream in("input.txt");
+    ofstream out("output.txt");
+
+    if (!in) {
+        cerr << "Errore nell'apertura del file input.txt" << endl;
+        return 1;
+    }
+
+    Graph graph;
+    graph.loadFromFile(in);
+
+    Node* start = graph.getNode(0); // esempio partenza da nodo 0
+    if (start != nullptr) {
+        graph.BFS(start, out);
+        graph.prim(start);
+        graph.printMST();
+    } else {
+        cout << "Nodo di partenza non trovato" << endl;
+    }
+
+    in.close();
+    out.close();
+    return 0;
 }
--- C++/es.cpp
+++ C++/es.cpp
@@ -1,97 +1,186 @@
 #include <iostream>
 #include <vector>
 #include <fstream>
+#include <climits>
+#include <queue>
+#include <sstream>
 
 using namespace std;
 
-class MaxHeap{
+enum Color { white, gray, black };
 
-    private:
-        vector<int> heap; // Vettore che rappresenta l'heap binario
+class Node {
+private:
+    Color color;
+    Node* predecessor;
+    vector<Node*> adj;
+    int distance;
+    int value;
 
-        int left(int i){
-            return 2*i + 1; // Indice del figlio sinistro di i
-        }
+public:
+    Node(int value) : value(value), predecessor(nullptr), color(white), distance(INT_MAX) {}
 
-        int right(int i){
-            return 2*i + 2; // Indice del figlio destro di i
-        }
+    int getValue() { return value; }
+    int getDistance() { return distance; }
+    Color getColor() { return color; }
+    vector<Node*> getAdj() { return adj; }
+    Node* getPredecessor() { return predecessor; }
 
-        int parent(int i){
-            return (i-1) / 2; // Indice del genitore del nodo i
-        }
-
-        void maxHeapify(int i, int heapSize){
-            int l = left(i);        // Calcolo indice del figlio sinistro
-            int r = right(i);       // Calcolo indice del figlio destro
-            int max = i;            // Inizialmente supponiamo che il massimo sia il nodo corrente
-
-            if(l < heapSize && heap[l] > heap[max])
-                max = l;            // Se il figlio sinistro esiste ed è maggiore, aggiorna max
-            if(r < heapSize && heap[r] > heap[max])
-                max = r;            // Se il figlio destro esiste ed è maggiore, aggiorna max
-            
-            if(max != i){
-                swap(heap[i], heap[max]);     // Se un figlio è maggiore, scambialo con il nodo corrente
-                maxHeapify(max, heapSize);    // Richiama ricorsivamente su max per mantenere proprietà heap
-            }
-        }
-
-    public:
-        MaxHeap(const vector<int>& data){
-            heap = data;           // Inizializza il vettore heap con i dati passati
-            buildMaxHeap();        // Costruisce l'heap a partire dai dati non ordinati
-        }
-
-        void buildMaxHeap(){
-            for(int i = heap.size() / 2 - 1; i >= 0; i--){ 
-                maxHeapify(i, heap.size());  // Applica maxHeapify ai nodi non foglia (da metà verso l'alto)
-            }
-        }
-
-        void heapSort(){
-            int heapSize = heap.size();      // Salva dimensione iniziale dell'heap
-
-            buildMaxHeap();                  // Ricostruisce l'heap (precauzione per sicurezza)
-
-            for(int i = heapSize - 1; i >= 1; i--){
-                swap(heap[0], heap[i]);      // Sposta il massimo (radice) in fondo all'array
-                heapSize--;                  // Riduce l'heap escludendo l'ultimo elemento ordinato
-                maxHeapify(0, heapSize);     // Ripristina proprietà heap sulla nuova radice
-            }
-        }
-
-        void printHeap() const{
-            for(int val: heap){
-                cout << val << " ";          // Stampa tutti gli elementi dell'heap
-            }
-            cout << endl;
-        }
-
+    void setAdj(Node* adia) { adj.push_back(adia); }
+    void setPredecessor(Node* pred) { predecessor = pred; }
+    void setColor(Color col) { color = col; }  // ✅ CORRETTO
+    void setDistance(int d) { distance = d; }
 };
 
+class Edge {
+private:
+    Node* src;
+    Node* dest;
+    int weights;
 
-int main(){
+public:
+    Edge(Node* u, Node* v, int w) : src(u), dest(v), weights(w) {}
 
+    Node* getSrc() { return src; }
+    Node* getDest() { return dest; }
+    int getWeights() { return weights; }
+
+    void setSrc(Node* u) { src = u; }
+    void setDest(Node* v) { dest = v; }
+    void setWeights(int w) { weights = w; }
+};
+
+class Graph {
+private:
+    vector<Node*> nodes;
+    vector<Edge*> edges;
+    int V, E;
+
+public:
+    Graph(int v = 0, int e = 0) : V(v), E(e) {}
+
+    Node* getNode(int value) {
+        for (auto& node : nodes) {
+            if (node->getValue() == value)
+                return node;
+        }
+        return nullptr;
+    }
+
+    Edge* getEdge(Node* src, Node* dest) {
+        for (auto& edge : edges) {
+            if (edge->getSrc() == src && edge->getDest() == dest)
+                return edge;
+        }
+        return nullptr;
+    }
+
+    void addEdge(Node* src, Node* dest, int weight) {
+        edges.push_back(new Edge(src, dest, weight));
+        src->setAdj(dest);
+        dest->setAdj(src); // se grafo orientato, togli questa
+        if (edges.size() > E) E = edges.size();
+    }
+
+    void addNode(Node* node) {
+        nodes.push_back(node);
+        if (nodes.size() > V) V = nodes.size();
+    }
+
+    void BFS(Node* src, ofstream& out) {
+        for (auto& node : nodes) {
+            node->setColor(white);
+            node->setPredecessor(nullptr);
+            node->setDistance(INT_MAX);
+        }
+
+        src->setDistance(0);
+        src->setColor(gray);
+
+        queue<Node*> q;
+        q.push(src);
+
+        while (!q.empty()) {
+            Node* node = q.front();
+            q.pop();
+
+            for (auto& adj : node->getAdj()) {
+                if (adj->getColor() == white) {
+                    adj->setPredecessor(node);
+                    adj->setDistance(node->getDistance() + 1);
+                    adj->setColor(gray);
+                    q.push(adj);
+                }
+            }
+
+            node->setColor(black);
+        }
+
+        for (auto& node : nodes) {
+            if (node->getPredecessor() == nullptr) {
+                out << "Node: " << node->getValue() << " -> (predecessor: NULL, distance: " << node->getDistance() << ")" << endl;
+            } else {
+                out << "Node: " << node->getValue()
+                    << " -> (predecessor: " << node->getPredecessor()->getValue()
+                    << ", distance: " << node->getDistance() << ")" << endl;
+            }
+        }
+    }
+
+    void loadFromFile(ifstream& input) {
+        int totalNodes, totalEdges;
+        string headerLine;
+
+        getline(input, headerLine);
+        if (headerLine.front() == '<') headerLine = headerLine.substr(1);
+        if (headerLine.back() == '>') headerLine.pop_back();
+        for (char& c : headerLine) if (c == ',') c = ' ';
+
+        istringstream headerStream(headerLine);
+        headerStream >> totalNodes >> totalEdges;
+
+        for (int i = 0; i < totalNodes; i++)
+            addNode(new Node(i));
+
+        string line;
+        while (getline(input, line)) {
+            if (line.front() == '<') line = line.substr(1);
+            if (line.back() == '>') line.pop_back();
+            for (char& c : line) if (c == ',') c = ' ';
+
+            istringstream edgeStream(line);
+            int srcVal, destVal, weight;
+            edgeStream >> srcVal >> destVal >> weight;
+
+            Node* src = getNode(srcVal);
+            Node* dest = getNode(destVal);
+            if (src && dest)
+                addEdge(src, dest, weight);
+        }
+    }
+};
+
+int main() {
     ifstream in("input.txt");
     ofstream out("output.txt");
 
-    vector<int> data;
-    int x;
+    if (!in.is_open()) {
+        cerr << "Errore nell'apertura del file input.txt" << endl;
+        return 1;
+    }
 
-   
-    while(in >> x)
-        data.push_back(x);
-    
-    MaxHeap heap(data);
-    
-    heap.printHeap();
+    Graph graph;
+    graph.loadFromFile(in);
 
-    heap.heapSort();
+    Node* start = graph.getNode(0); // nodo di partenza
 
-    cout << "Heap dopo il sort"<<endl;
-    heap.printHeap();
+    if (start != nullptr) {
+        graph.BFS(start, out);
+    } else {
+        cerr << "Nodo di partenza non trovato." << endl;
+    }
 
+    in.close();
+    out.close();
     return 0;
-
 }
